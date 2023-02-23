16662693d9fab4f1f8cee0c43673719516d1e976
epbrooks <123840563+epbrooks@users.noreply.github.com>
2023-02-22 20:43:56 -0800

    Added more variables in steps

---
+++ features/steps/amazon_bestsellers.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+
+
+#@given('Open Amazon Bestsellers Page')
+    #def open_amazon_bestsellers(context):
+        #context.driver.get('https://www.amazon.com/gp/bestsellers/?ref_=nav_cs_bestsellers')
+
+#@then('Verify Header links are present')
+    #def verify_header_links(context)
+        #context.driver.find_element
--- features/steps/amazon_cart.py
+++ features/steps/amazon_cart.py
@@ -3,40 +3,47 @@ from behave import given, when, then
 
 SEARCH_INPUT = (By.NAME, 'q')
 SEARCH_SUBMIT = (By.NAME, 'btnK')
+EMPTY_CART = (By.CSS_SELECTOR, 'span#nav-cart-count')
+SEARCH_WORD = (By.CSS_SELECTOR, 'input#twotabsearchtextbox')
+CLICK_TABLET = (By.CSS_SELECTOR, "div[data-cel-widget='search_result_2'] span.a-size-medium")
+CLICK_ADD_TO_CART = (By.CSS_SELECTOR, 'input#add-to-cart-button')
+CLICK_NO_THANKS = (By.CSS_SELECTOR, 'span.abb-intl-decline')
+CLICK_GO_TO_CART = (By.CSS_SELECTOR, "a[href='/gp/cart/view.html?ref_=sw_gtc']")
+VERIFY_CART_ITEM = (By.CSS_SELECTOR, 'span#sc-subtotal-label-activecart')
 
 
 @then('Verify that Amazon Cart is empty')
 def verify_amazon_cart_empty(context):
     expected_text = '0'
-    actual_text = context.driver.find_element(By.CSS_SELECTOR, 'span#nav-cart-count').text
+    actual_text = context.driver.find_element(*EMPTY_CART).text
     assert expected_text == actual_text, f'Expected {expected_text} but got {actual_text}.'
 
 @when('Input Tablets into search field')
 def input_search_word(context):
-    context.driver.find_element(By.CSS_SELECTOR, 'input#twotabsearchtextbox').send_keys('tablets')
+    context.driver.find_element(*SEARCH_WORD).send_keys('tablets')
 
 
 @when('Click on Amazon Fire HD 10 inch tablet')
 def click_amazon_tablet(context):
-    context.driver.find_element(By.CSS_SELECTOR, "div[data-cel-widget='search_result_2'] span.a-size-medium").click()
+    context.driver.find_element(*CLICK_TABLET).click()
 
 
 @when('Click on Add to Cart')
 def click_add_to_cart(context):
-    context.driver.find_element(By.CSS_SELECTOR, 'input#add-to-cart-button').click()
+    context.driver.find_element(*CLICK_ADD_TO_CART).click()
 
 @when('Click on No Thanks')
 def click_no_thanks(context):
-    context.driver.find_element(By.CSS_SELECTOR, 'span.abb-intl-decline').click()
+    context.driver.find_element(*CLICK_NO_THANKS).click()
 
 
 @when('Click on Go to Cart')
 def click_go_to_cart(context):
-    context.driver.find_element(By.CSS_SELECTOR, "a[href='/gp/cart/view.html?ref_=sw_gtc']").click()
+    context.driver.find_element(*CLICK_GO_TO_CART).click()
 
 
 @then('Verify that Amazon Cart contains selected item')
 def verify_cart_contains_item(context):
     expected_text = 'Subtotal (1 item):'
-    actual_text = context.driver.find_element(By.CSS_SELECTOR, 'span#sc-subtotal-label-activecart').text
+    actual_text = context.driver.find_element(*VERIFY_CART_ITEM).text
     assert actual_text == expected_text, f'Expected {expected_text} but got {actual_text}'
