ae1f9d084f5fef00f3fc3f151dd308a29a699d01
epbrooks <123840563+epbrooks@users.noreply.github.com>
2023-03-04 13:41:24 -0800

    Added assertion for HW5

--- features/steps/product_page_steps.py
+++ features/steps/product_page_steps.py
@@ -7,7 +7,7 @@ from time import sleep
 #PRODUCT_NAME =
 #PRODUCT_PRICE =
 COLOR_OPTIONS = (By.CSS_SELECTOR, '#variation_color_name li')
-#CURRENT_COLOR =
+CURRENT_COLOR = (By.CSS_SELECTOR, '#variation_color_name .selection')
 
 @given('Open Amazon product {product_id} page')
 def open_product(context, product_id):
@@ -19,6 +19,14 @@ def verify_user_can_select_colors(context):
     context.driver.find_element(*COLOR_OPTIONS).click()
 
     all_color_options = context.driver.find_elements(*COLOR_OPTIONS)
+    print('All colors: ', all_color_options)
+    expected_colors = ['Bright Cobalt', 'Maroon', 'Navy', 'True Black', 'Ash Green']
 
-    for color in all_color_options:
+    actual_colors = []
+    for color in all_color_options[:5]:
         color.click()
+        current_color = context.driver.find_element(*CURRENT_COLOR).text
+        print('Current color: ', current_color)
+        actual_colors += [current_color]
+
+        assert expected_colors == actual_colors, f'Expected {expected_colors}, but got {actual_colors}'
